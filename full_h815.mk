e145f2fadf8ad5a562a93407d56e12eb80435a3d
steadfasterX <steadfastX@boun.cr>
2018-03-16 11:49:44 +0100

    port to UsU

--- full_h815.mk
+++ full_h815.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from h815 device
-$(call inherit-product, device/lge/h815/device.mk)
+$(call inherit-product, device/lge/h815_usu/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := h815
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,5 +2,5 @@ export ANDROID_SET_JAVA_HOME=true
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export USE_NINJA=true
 
-add_lunch_combo aoscp_h815-userdebug
-add_lunch_combo aoscp_h815-eng
+add_lunch_combo aoscp_h815_usu-userdebug
+add_lunch_combo aoscp_h815_usu-eng
