5f9693591716c13a7e66032985545b08be49298f
steadfasterX <steadfastX@boun.cr>
2018-04-17 16:51:31 +0200

    typo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 export ANDROID_SET_JAVA_HOME=true
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export USE_NINJA=true
-export CCACHE=1
+export USE_CCACHE=1
 
 add_lunch_combo lineage_h815_usu-userdebug
 add_lunch_combo lineage_h815_usu-eng
